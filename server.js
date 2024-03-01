a992eea5569b314d6d65574489ff0dae872604eb
Natalia392 <ntorrejonvalencia@gmail.com>
2024-02-29 22:28:10 -0300

    fix: auth route headers Typo, and methods. Cors options updated.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const db = require('./app/models');
 
 const corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
